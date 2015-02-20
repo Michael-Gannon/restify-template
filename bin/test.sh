6b68ba6608e130031ae06bcd2dfae31731d59d3f
Michael Gannon <michael.gannon@gmail.com>
2015-02-20 22:28:15 +1100

    can't someone else do it? Yes! Bash can do it!

--- bin/test.sh
+++ bin/test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+set -e
 
 echo "checking code quality"
-./node_modules/.bin/jshint index.js ./app/ --reporter unix && #lint
-./node_modules/.bin/jscs index.js ./app/ && #code standards
+./node_modules/.bin/jshint index.js ./app/ --reporter unix
+./node_modules/.bin/jscs index.js ./app/
 
 printf "\nrunning unit tests"
-./bin/test-unit.sh &&
+./bin/test-unit.sh
 
 printf "\nchecking test coverage\n"
-./node_modules/.bin/istanbul --config=./test/istanbul-config.json check-coverage &&
+./node_modules/.bin/istanbul --config=./test/istanbul-config.json check-coverage
 
 printf "\nrunning acceptance tests\n"
 ./bin/test-acceptance.sh
